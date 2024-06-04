39d7d802a02ed228ce9d42af2aeea8e1a971bcb3
Sneha_Khandar <khandarsneha2004@gmail.com>
2024-06-04 22:54:05 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,66 @@
+// script.js
+const celsiusField = document.querySelector("#celsius");
+const degree = document.querySelector("#degree");
+const convertBtn = document.querySelector("#convert-btn");
+const tempType = document.querySelector("#temp-type");
+
+
+function updateTime() {
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+
+  var ampm = hours >= 12 ? "PM" : "AM";
+
+  // Convert hours to 12-hour format
+  hours = hours % 12;
+  hours = hours ? hours : 12; // "0" will be converted to 12
+
+  // Add leading zeros if needed
+  hours = (hours < 10 ? "0" : "") + hours;
+  minutes = (minutes < 10 ? "0" : "") + minutes;
+  seconds = (seconds < 10 ? "0" : "") + seconds;
+
+  var timeString = hours + ":" + minutes + ":" + seconds + " " + ampm;
+
+  document.getElementById("time").innerText = timeString;
+}
+setInterval(updateTime, 1000);
+updateTime();
+
+window.addEventListener("load", () => {
+  degree.value = "";
+  celsiusField.innerHTML = "";
+});
+
+if(degree.value === ""){
+  convertBtn.setAttribute("disabled","");
+  setTimeout(() => {
+    convertBtn.removeAttribute('disabled');
+  }, 4000);
+}
+
+
+convertBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  convertToCelsius();
+  convertBtn.innerHTML = "<span class='icon'><i class='fa fa-spinner fa-spin'></i> Converting...</span>";
+  setTimeout(() => {
+    convertBtn.innerHTML ="<span>Convert</span>"
+  }, 1000);
+});
+
+function convertToCelsius() {
+  let inputValue = degree.value;
+  
+  setTimeout( () => {
+    if (tempType.value === "fahrenheit") {
+      const FahrenheitToCelsius = (inputValue - 32) * (5 / 9);
+      celsiusField.innerHTML = `${FahrenheitToCelsius.toFixed(3)} &deg;c`;
+    } else if (tempType.value === "kelvin") {
+      const KelvinToCelsius = inputValue - 273.15;
+      celsiusField.innerHTML = `${KelvinToCelsius.toFixed(3)} &deg;c`;
+    }
+  }, 1200)
+}
